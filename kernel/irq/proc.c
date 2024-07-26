F1xy-kernels/RAPHAEL_Kernel

kernel: irq: properly disallow userspace from changing IRQs affinity
Setting IRQD_AFFINITY_MANAGED flag on msm-4.4 is fine, it only
disallows userspace from touching IRQ's affinity. But on 4.14
it also prevents an IRQ from migrating off a dying core.

Fix that by disallowing userspace from changing affinities of
perf critical IRQs directly.

Signed-off-by: Yaroslav Furman <yaro330@gmail.com>

kernel/irq/proc.c
@@ -144,10 +144,14 @@ static ssize_t write_irq_affinity(int type, struct file *file,
 		const char __user *buffer, size_t count, loff_t *pos)
 {
 	unsigned int irq = (int)(long)PDE_DATA(file_inode(file));
+	struct irq_desc *desc = irq_to_desc(irq);
 	cpumask_var_t new_value;
 	int err;
 
 	if (!irq_can_set_affinity_usr(irq) || no_irq_affinity)
+		return -EIO;
+
+	if (!irqd_has_set(&desc->irq_data, IRQF_PERF_CRITICAL))
 		return -EIO;
 
 	if (!alloc_cpumask_var(&new_value, GFP_KERNEL))

saiffdaura/biofrost_kernel_realme

Signed-off-by: Cyber Knight <cyberknight755@gmail.com>
Signed-off-by: mcdofrenchfreis <xyzevan@androidist.net>

kernel/irq/proc.c
@@ -144,10 +144,14 @@ static ssize_t write_irq_affinity(int type, struct file *file,
 		const char __user *buffer, size_t count, loff_t *pos)
 {
 	unsigned int irq = (int)(long)PDE_DATA(file_inode(file));
+	struct irq_desc *desc = irq_to_desc(irq);
 	cpumask_var_t new_value;
 	int err;
 
 	if (!irq_can_set_affinity_usr(irq) || no_irq_affinity)
+		return -EIO;
+
+	if (!irqd_has_set(&desc->irq_data, IRQD_PERF_CRITICAL))
 		return -EIO;
 
 	if (!alloc_cpumask_var(&new_value, GFP_KERNEL))